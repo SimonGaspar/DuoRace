e0bd62991d3965464157db8c503ced17214678bf
SimonGaspar <simon.gaspar@gmail.com>
2020-01-18 13:21:23 +0100

    Init commit

---
+++ Assets/_Prototype/Scripts/NavMeshAgentMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class NavMeshAgentMove : MonoBehaviour
+{
+    public List<Transform> goals = new List<Transform>();
+    public float rotationStrength = 1f;
+    public float distanceOfPoint = 30;
+
+    private Transform goal;
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    private int iter = 0;
+
+    void Awake() 
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        goal = goals.First();
+        agent.destination = goal.position;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(goal.position, transform.position)<distanceOfPoint) {
+            iter = ++iter == goals.Count ? 0 : iter;
+            goal = goals[iter];
+            agent.destination = goal.position;
+        }
+    }
+}
